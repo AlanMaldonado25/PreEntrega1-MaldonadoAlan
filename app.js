0de27bf95aa5f8a8ff95a57c936a6d5457cca5a5
Alan Maldonado <alanmaldo.ct@gmail.com>
2023-08-15 18:47:40 -0300

    Optimizando codigo

--- app.js
+++ app.js
@@ -11,22 +11,18 @@ const resultadoSemana = calculo(horasAlDia,diasSemana,sueldoPorHora);
 alert(`A la semana estas ganando $${resultadoSemana}`)
 
 
-let pregunta = prompt('Quieres saber cuanto ganas al mes?');
+let pregunta = prompt('Quieres saber cuanto ganas al mes?(Si/No)');
+function sueldoMes(sueldoSemana, semTrabajadas){
+        
+    const alMes = sueldoSemana * semTrabajadas;
+    return alMes;
+}
 
 if(pregunta === "si"|| pregunta === 'SI' || pregunta === 'Si'){
     const sueldoSemana = resultadoSemana;
     const semTrabajadas = parseFloat(prompt('Cuantas semanas trabajaste este mes?'));
-    
-    function sueldoMes(sueldoSemana, semTrabajadas){
-        
-        const alMes = sueldoSemana * semTrabajadas;
-        return alMes;
-    }
     const resultado = sueldoMes(sueldoSemana,semTrabajadas);
     alert(`El total ganado al mes es $${resultado}`);
-    
-
-    
 }else{
     alert('Gracias eso fue todo.');
 }
